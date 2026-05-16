34fd94c42ec22080c3b23991fb1fbb80b502efd3
bsantillan <brunosaantillan@gmail.com>
2026-05-15 22:36:39 -0300

    feat: Se eliminaron los tests

--- src/test/java/com/LosCiruelos/padel_club_api/PadelClubApiApplicationTests.java
+++ src/test/java/com/LosCiruelos/padel_club_api/PadelClubApiApplicationTests.java
@@ -1,11 +1,9 @@
 package com.LosCiruelos.padel_club_api;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled
 class PadelClubApiApplicationTests {
 
 	@Test
